d8072bd575e5780bbfcf35c888750a07503120fe
rimever <mot_siva_ordin@hotmail.com>
2020-07-19 13:54:19 +0900

    3章まで進めた

---
+++ Chapter1/index.js
@@ -0,0 +1,8 @@
+var http = require('http');
+
+http.createServer(function (request,response) {
+    response.writeHead(200, {'Content-Type':'text/plain'});
+    response.write('Hello World');
+    response.end();
+}).listen(3000);
+console.log('open http://localhost:3000');
